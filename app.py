26ded78910b4f0ae6cf13c810886886f18568b08
kivistikliis <liis.kivistik@cytosmart.com>
2018-05-16 23:11:51 +0200

    Revert "try 3"
    This reverts commit ac73381789673dce99796397820afeef499b7efa.

--- app.py
+++ app.py
@@ -18,6 +18,5 @@ import controllers.event_controller
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    app.run()
-    print("run")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
