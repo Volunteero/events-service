c6f5f7a7d549951169425235d4b7c50d773fb9ec
kivistikliis <liis.kivistik@cytosmart.com>
2018-05-16 22:45:39 +0200

    try heroku

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ client = MongoClient(MONGO_URL)
 db = client.get_default_database()
 
 import controllers.event_controller
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
