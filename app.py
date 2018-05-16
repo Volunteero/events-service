ac73381789673dce99796397820afeef499b7efa
kivistikliis <liis.kivistik@cytosmart.com>
2018-05-16 23:09:08 +0200

    try 3

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ import controllers.event_controller
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run()
+    print("run")
