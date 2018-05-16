bc6ee9bd657009a5f93971572f94773ea7632aa6
kivistikliis <liis.kivistik@cytosmart.com>
2018-05-16 22:59:31 +0200

    Revert "try 2"
    This reverts commit bdef576be19d8cb60e78b7cdad863669fc6db5c1.

--- app.py
+++ app.py
@@ -19,5 +19,4 @@ import controllers.event_controller
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    host = os.environ.get('HOST')
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=port)
